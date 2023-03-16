6b2c381f028a4aca7f926edf076396ea72c56b69
akhilsoman1234 <116093435+akhilsoman1234@users.noreply.github.com>
2023-03-16 11:13:39 +0530

    user

--- resources/views/Account/users.blade.php
+++ resources/views/Account/users.blade.php
@@ -232,7 +232,7 @@ box-sizing: border-box;
 												<thead>
 													<!--begin::Table row-->
 													<tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-													<th class="min-w-100px">#</th>
+													<th class="min-w-40px">#</th>
 														<th class="min-w-125px">Full Name</th>
 														<th class="min-w-125px">Email</th>
 														<th class="min-w-125px">Role</th>
